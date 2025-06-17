15d5b930ad26203867bf3c3d17332b30b17d8a68
外村剛一 <kou91515@gmail.com>
2025-06-17 09:56:27 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+function squareNumber(number) {
+    // 引数として受け取った数値を二乗します
+    let result = number * number;
+    // 計算結果を返します
+    return result;
+}
+
+// 例として引数に10を渡して関数を実行します
+let result = squareNumber(10);
+
+// 計算結果をコンソールに出力します
+console.log(result);
