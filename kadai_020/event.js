6c44de289980b6ff6fce7a6feb340c1d5a8cf53c
外村剛一 <kou91515@gmail.com>
2025-06-18 10:05:31 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,3 +1,5 @@
-document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'クリック後の表示';
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+btn.addEventListener('click', function() {
+    text.textContent = 'クリック後の表示';
 });
